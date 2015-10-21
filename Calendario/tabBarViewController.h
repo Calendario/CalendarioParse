a553f2328b71bc915aa8c99bc8cadc58cbbf471d
LarryBrian <LarryBrian@larrys-macbook-pro.local>
2015-10-20 22:34:38 -0400

    UI Tab Bar VC added including .h and .m files

---
+++ Calendario/tabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  tabBarViewController.h
+//  Calendario
+//
+//  Created by Larry B. King on 10/20/15.
+//  Copyright © 2015 Calendario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tabBarViewController : UITabBarController
+
+@end
