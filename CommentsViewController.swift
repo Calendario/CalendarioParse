6756c146af3d77ebe9198ac5ecfbbc97cea8170b
Derek Cacciotti <applegamer247@gmail.com>
2015-10-27 12:47:37 -0400

    Started Working on the comments view
    Created VC for comments view

---
+++ CommentsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  CommentsViewController.swift
+//  Calendario
+//
+//  Created by Derek Cacciotti on 10/27/15.
+//  Copyright © 2015 Calendario. All rights reserved.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var commentTextView: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // table view methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
