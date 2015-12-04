7d0eb37201d983e101b719f0fb2e1b31a9378478
LarryBrian <LarryBrian@larrys-mbp>
2015-12-04 15:55:01 -0600

    Location ViewController UI implemented (no code yet)

---
+++ Calendario/locationViewController.h
@@ -0,0 +1,13 @@
+//
+//  locationViewController.h
+//  Calendario
+//
+//  Created by Larry B. King on 12/4/15.
+//  Copyright © 2015 Calendario. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface locationViewController : UIViewController
+
+@end
