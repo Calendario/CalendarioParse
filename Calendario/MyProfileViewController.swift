12cac7bda2c44625d5279f31838e27c41553cc41
Daniel Sadjadian <supertecnoboff@me.com>
2015-12-13 17:34:09 +0000

    Block User - My Profile Page - Added support for blocking users + bug fixes and minor changes.

--- Calendario/MyProfileViewController.swift
+++ Calendario/MyProfileViewController.swift
@@ -27,18 +27,23 @@ class MyProfileViewController : UIViewController {
     @IBOutlet weak var profileScroll: UIScrollView!
     @IBOutlet weak var backButton: UIBarButtonItem!
     @IBOutlet weak var settingsButton: UIBarButtonItem!
-    
+    @IBOutlet weak var blockedBlurView: UIView!
+    @IBOutlet weak var blockedViewDesc: UITextView!
     
     @IBOutlet weak var FollowButton: UIButton!
     
     // Follow method property
     var FollowObject = FollowHelper()
-        
     
     // Do NOT change the following line of
     // code as it MUST be set to PUBLIC.
     public var passedUser:PFUser!
     var userID:String!
+    
+    // User block check.
+    // 1 = You blocked the user.
+    // 2 = User has blocked you.
+    var blockCheck:Int = 0
     
     // User website link.
     var userWebsiteLink:String!
@@ -97,12 +102,187 @@ class MyProfileViewController : UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func openSettingsOrMoreSection(sender: UIButton) {
+        
+        // If the current user is being displayed then show the user 
+        // settings menu otherwise display the more action sheet.
+        
+        if (passedUser == nil) {
+            
+            // No user passed in - show the settings menu.
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let viewC = storyboard.instantiateViewControllerWithIdentifier("SettingsView") as! SettingsViewController
+            self.presentViewController(viewC, animated: true, completion: nil)
+        }
+            
+        else {
+            
+            if (userID != nil) {
+                
+                var alertDesc:String!
+                var buttonOneTitle:String!
+                
+                if (blockCheck == 1) {
+                    
+                    alertDesc = "Unblock or report the user dislayed user account."
+                    buttonOneTitle = "Unblock User"
+                }
+                    
+                else {
+                    
+                    alertDesc = "Block or report the user dislayed user account."
+                    buttonOneTitle = "Block User"
+                }
+                
+                // User passed in - show the more menu.
+                let moreMenu = UIAlertController(title: "Options", message: alertDesc, preferredStyle: .ActionSheet)
+                
+                // Setup the alert actions.
+                let blockUser = { (action:UIAlertAction!) -> Void in
+                    
+                    // Check if the user has already been blocked
+                    // and then show the appropriate actions.
+                    var query = PFQuery(className: "blockUser")
+                    query.whereKey("userBlock", equalTo: self.passedUser)
+                    query.whereKey("userBlocking", equalTo: PFUser.currentUser()!)
+                    query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+                        
+                        if (error == nil) {
+                            
+                            if (objects?.count > 0) {
+                                
+                                // The user has already been blocked.
+                                let unblockAlert = UIAlertController(title: "Unblock user?", message: "You have already blocked this user. Would you like to unblock this user?", preferredStyle: .Alert)
+                                
+                                // Setup the alert actions.
+                                let unblockUser = { (action:UIAlertAction!) -> Void in
+                                    
+                                    // As all users are unique and there is only a 1-1
+                                    // relationship in each user block request we only 
+                                    // ever need to delete the first object from the array.
+                                    let objects = objects as [PFObject]!
+                                    
+                                    // Submit the unblock request.
+                                    objects[0].deleteInBackgroundWithBlock({ (success: Bool, error: NSError?) -> Void in
+                                        
+                                        if (success) {
+                                            
+                                            // Set to check to the default.
+                                            self.blockCheck = 0
+                                            
+                                            // The user has been unblocked.
+                                            self.blockedBlurView.alpha = 0.0
+                                        }
+                                        
+                                        else {
+                                            
+                                            // An error has occured.
+                                            self.displayAlert("Error", alertMessage: "\(error?.description)")
+                                        }
+                                    })
+                                }
+                                
+                                // Setup the alert buttons.
+                                let yes = UIAlertAction(title: "Yes", style: .Default, handler: unblockUser)
+                                let cancel = UIAlertAction(title: "No", style: .Default, handler: nil)
+                                
+                                // Add the actions to the alert.
+                                unblockAlert.addAction(yes)
+                                unblockAlert.addAction(cancel)
+                                
+                                // Present the alert on screen.
+                                self.presentViewController(unblockAlert, animated: true, completion: nil)
+                            }
+                            
+                            else {
+                                
+                                // The user isn't currently blocked
+                                // therefore proceed with blocking.
+                                
+                                // Set the user to block and the
+                                // user which is blocking that user.
+                                var blockUserData = PFObject(className:"blockUser")
+                                blockUserData["userBlock"] = self.passedUser
+                                blockUserData["userBlocking"] = PFUser.currentUser()
+                                
+                                // Submit the block request.
+                                blockUserData.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                                    
+                                    // Check if the user block data
+                                    // request was succesful or not.
+                                    
+                                    if (success) {
+                                        
+                                        // The user has been blocked.
+                                        let blockUpdate = UIAlertController(title: "Success", message: "The user has been blocked.", preferredStyle: .Alert)
+                                        
+                                        // Setup the alert actions.
+                                        let close = { (action:UIAlertAction!) -> Void in
+                                            self.dismissViewControllerAnimated(true, completion: nil)
+                                        }
+                                        
+                                        // Setup the alert buttons.
+                                        let dismiss = UIAlertAction(title: "Dismiss", style: .Default, handler: close)
+                                        
+                                        // Add the actions to the alert.
+                                        blockUpdate.addAction(dismiss)
+                                        
+                                        // Present the alert on screen.
+                                        self.presentViewController(blockUpdate, animated: true, completion: nil)
+                                    }
+                                        
+                                    else {
+                                        
+                                        // There was a problem, check error.description.
+                                        self.displayAlert("Error", alertMessage: "\(error?.description)")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                let reportUser = { (action:UIAlertAction!) -> Void in
+                }
+                
+                // Setuo the alert buttons.
+                let buttonOne = UIAlertAction(title: buttonOneTitle, style: .Default, handler: blockUser)
+                let buttonTwo = UIAlertAction(title: "Report User", style: .Default, handler: reportUser)
+                let cancel = UIAlertAction(title: "Dismiss", style: .Cancel, handler: nil)
+                
+                // Add the actions to the alert.
+                moreMenu.addAction(buttonOne)
+                moreMenu.addAction(buttonTwo)
+                moreMenu.addAction(cancel)
+                
+                // Present the alert on screen.
+                presentViewController(moreMenu, animated: true, completion: nil)
+            }
+                
+            else {
+                self.displayAlert("Error", alertMessage: "You cannot view the more options menu as no user ID has been detected.")
+            }
+        }
+    }
+    
     // View Did Load method.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Get the screen dimensions.
+        let width = UIScreen.mainScreen().bounds.size.width
+        
+        // Add the blocked blur view to the view.
+        blockedBlurView.frame = CGRectMake(0, 0, width, self.profileScroll.bounds.size.height)
+        blockedBlurView.alpha = 0.0
+        self.profileScroll.addSubview(blockedBlurView)
+        
+        // Add the blur to the blocked view.
+        var visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .Dark)) as UIVisualEffectView
+        visualEffectView.frame = self.blockedBlurView.bounds
+        blockedBlurView.insertSubview(visualEffectView, atIndex: 0)
     }
     
     // View Did Appear method.
@@ -110,11 +290,22 @@ class MyProfileViewController : UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
+        // By default the more button is diabled until
+        // we have downloaded the appropriate user data.
+        settingsButton.enabled = false
+        self.blockCheck = 0
+        
+        // Disable scrolling access.
+        self.profileScroll.userInteractionEnabled = false
+        
         // Check to see if a user is being passed into the
         // view controller and run the appropriate actions.
         var currentUser:PFUser!
         
         if (passedUser == nil) {
+            
+            // Allow scrolling access.
+            self.profileScroll.userInteractionEnabled = true
             
             // Show the currently logged in user.
             currentUser = PFUser.currentUser()
@@ -129,9 +320,58 @@ class MyProfileViewController : UIViewController {
             // Show the settings button.
             settingsButton.enabled = true
             settingsButton.image = UIImage(named: "SettingsV2.png")
+            settingsButton.title = nil
         }
             
         else {
+            
+            // Check if the user has already been blocked
+            // or if the user has blocked you and take
+            // the appropriate actions.
+            var query = PFQuery(className: "blockUser")
+            query.whereKey("userBlock", equalTo: self.passedUser)
+            query.whereKey("userBlocking", equalTo: PFUser.currentUser()!)
+            query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+                
+                if (error == nil) {
+                    
+                    if (objects?.count > 0) {
+                        
+                        // User has been blocked so
+                        // display blocked blur view.
+                        self.blockedBlurView.alpha = 1.0;
+                        self.blockedViewDesc.text = "You have blocked this user. Tap the button in the top right hand corner to unblock."
+                        self.settingsButton.enabled = true
+                        self.blockCheck = 1
+                    }
+                    
+                    else {
+                        
+                        var queryTwo = PFQuery(className: "blockUser")
+                        queryTwo.whereKey("userBlock", equalTo: PFUser.currentUser()!)
+                        queryTwo.whereKey("userBlocking", equalTo: self.passedUser)
+                        queryTwo.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+                            
+                            if (error == nil) {
+                                
+                                if (objects?.count > 0) {
+                                    
+                                    // The user has blocked you.
+                                    self.blockedBlurView.alpha = 1.0
+                                    self.blockedViewDesc.text = "This user has blocked you."
+                                    self.blockCheck = 2
+                                }
+                                
+                                else {
+                                    
+                                    // Allow access to the more button.
+                                    self.settingsButton.enabled = true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             
             // Show the user being passed into the view.
             currentUser = passedUser
@@ -146,9 +386,10 @@ class MyProfileViewController : UIViewController {
             // Set the edit button text.
             self.editButton.setTitle("Follow \(userID)", forState: UIControlState.Normal)
             
-            // Hide the settings button.
-            settingsButton.enabled = false
+            // Change the setting button to
+            // the more actions button.
             settingsButton.image = nil
+            settingsButton.title = "More"
         }
         
         // Notify the user that the app is loading.
