ebe52c6b0974e5ff575719a750ee3d53967031d4
Aris <social@live.com.my>
2016-03-22 23:41:21 +0800

    Interview Implementation completed but will need Larry's help on Auto-layout constraints

---
+++ IntrochildViewController.swift
@@ -0,0 +1,104 @@
+//
+//  IntrochildViewController.swift
+//  Calendario
+//
+//  Created by Harith Bakri on 22/03/2016.
+//  Copyright © 2016 Calendario. All rights reserved.
+//
+
+import UIKit
+
+class IntrochildViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate, LastIntroViewControllerDelegate {
+    
+    var pageViewController: UIPageViewController!
+    let pages = ["Pageone","Pagetwo","Pagethree"]
+    
+    
+    // MARK: LastIntroViewControllerDelegate methods
+    
+    func lastPageDone() {
+        print("View Controller says Last page done")
+        
+        if let appVC = storyboard?.instantiateViewControllerWithIdentifier("LoginPage") {
+            let currentVC = pageViewController
+            
+            appVC.view.frame = currentVC.view.frame
+            appVC.willMoveToParentViewController(self)
+            addChildViewController(appVC)
+            
+            transitionFromViewController(currentVC, toViewController: appVC, duration: 1.0, options: .TransitionCrossDissolve, animations: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, completion: { (completed: Bool) -> Void in
+                currentVC.removeFromParentViewController()
+                appVC.didMoveToParentViewController(self)
+            })
+            
+            
+        }
+        
+    }
+    
+    // MARK: Page View Controller Datasource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        if let index = pages.indexOf(viewController.restorationIdentifier!) {
+            
+            if index > 0 {
+                return viewControllerAtIndex(index - 1)
+            }
+        }
+        
+        return nil
+    }
+    
+
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        if let index = pages.indexOf(viewController.restorationIdentifier!) {
+            
+            if index < pages.count - 1 {
+                return viewControllerAtIndex(index + 1)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    func viewControllerAtIndex(index: Int) -> UIViewController? {
+        let vc = storyboard?.instantiateViewControllerWithIdentifier(pages[index])
+        
+        if pages[index] == "Pagethree" {
+            
+            (vc as! LastIntroViewController).delegate = self
+            
+        }
+        
+        
+        return vc
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let vc = storyboard?.instantiateViewControllerWithIdentifier("IntroPageViewController") {
+            self.addChildViewController(vc)
+            self.view.addSubview(vc.view)
+            
+            pageViewController = vc as! UIPageViewController
+            pageViewController.dataSource = self
+            pageViewController.delegate = self
+            
+            pageViewController.setViewControllers([viewControllerAtIndex(0)!], direction: .Forward, animated: true, completion: nil)
+            pageViewController.didMoveToParentViewController(self)
+            
+        }
+       
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ LastIntroViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LastIntroViewController.swift
+//  Calendario
+//
+//  Created by Harith Bakri on 22/03/2016.
+//  Copyright © 2016 Calendario. All rights reserved.
+//
+
+import UIKit
+
+protocol LastIntroViewControllerDelegate {
+    func lastPageDone()
+}
+
+class LastIntroViewController: UIViewController {
+    
+    var delegate: LastIntroViewControllerDelegate?
+
+    
+    // MARK: IBActions
+    @IBAction func doneButtonTapped(sender: UIButton) {
+        
+        if delegate != nil {
+            delegate?.lastPageDone()
+        }
+        
+    }
+}
