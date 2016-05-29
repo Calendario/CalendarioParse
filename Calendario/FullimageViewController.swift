742c8176d84c9ac5a7547d400302eb85b9594aa3
Aris <social@live.com.my>
2016-05-29 22:01:35 +0800

    Fixed the call from Newsfeed to FullImage View but need help with making call to newsfeed from FullImage View

--- Calendario/FullimageViewController.swift
+++ Calendario/FullimageViewController.swift
@@ -16,10 +16,9 @@ class FullimageViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBAction func back(sender: UIBarButtonItem) {
+    @IBAction func BackButtontapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
-    
     
     
     let zoomImageView = UIImageView()
@@ -29,7 +28,6 @@ class FullimageViewController: UIViewController, UIScrollViewDelegate {
     public var passedImage:UIImage!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         
         self.scrollView.minimumZoomScale = 1.0
@@ -38,38 +36,7 @@ class FullimageViewController: UIViewController, UIScrollViewDelegate {
        
         self.Image.image = passedImage
         
-        
-        
-        
-        zoomImageView.frame = CGRectMake(0, 0, 200, 100)
-        zoomImageView.backgroundColor = UIColor.redColor()
-       // zoomImageView.image = UIImage(data: passedImage)
-        zoomImageView.contentMode = .ScaleAspectFill
-        zoomImageView.clipsToBounds = true
-        
-        zoomImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "animated"))
-        
-    
-        view.addSubview(zoomImageView)
-        
-        
     }
-    
-    func animate() {
-        UIView.animateWithDuration(0.75) { () -> Void in
-            
-            let height = (self.view.frame.width / self.startingFrame.width) * self.startingFrame.height
-            
-            let y = self.view.frame.height / 2 - height / 2
-            
-            self.zoomImageView.frame = CGRectMake(0 , 0, self.view.frame.width, 100)
-        }
-    }
-
-    
-    
-    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resource that can be recreated.
