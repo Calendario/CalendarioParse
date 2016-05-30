a737abbb907e92c458f87530eb4dd7a705bdaaed
Aris <social@live.com.my>
2016-05-30 17:01:38 +0800

    Improvised Fullimage View Controller so that zooming picture is optimised.

--- Calendario/FullimageViewController.swift
+++ Calendario/FullimageViewController.swift
@@ -13,16 +13,18 @@ class FullimageViewController: UIViewController, UIScrollViewDelegate {
     
     
     @IBOutlet weak var Image: UIImageView!
-    
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBAction func BackButtontapped(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     
-    let zoomImageView = UIImageView()
-    let startingFrame = CGRectMake(0, 0, 200, 100)
+ //   let zoomImageView = UIImageView()
+ //   let startingFrame = CGRectMake(0, 0, 200, 100)
     
     
     public var passedImage:UIImage!
@@ -44,5 +46,40 @@ class FullimageViewController: UIViewController, UIScrollViewDelegate {
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return self.Image
+    }
+    
+    private func updateMinZoomScaleForSize(size: CGSize) {
+        
+        let widthScale = size.width / Image.bounds.width
+        let heightScale = size.height / Image.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.zoomScale = minScale
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        updateMinZoomScaleForSize(view.bounds.size)
+    }
+    
+    private func updateViewConstraints(size: CGSize) {
+        
+        let yOffset = max(0, (size.height - Image.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - Image.frame.height) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+        
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateViewConstraints()
+        
     }
 }
